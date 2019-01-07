a9bcb9dafebef139e0894070b5e61e15928be675
asokarimi <asokarimi.web@gmail.com>
2019-01-07 16:36:54 +0330

    test

--- app.js
+++ app.js
@@ -1,3 +1,21 @@
 'use strict';
 
-console.log('hello');
+function SimpleAdding (num) {
+  let sum = 0;
+  for (let i = 1; i <= num; i++) {
+    sum += i;
+  }
+  return sum;
+}
+console.log(SimpleAdding('140'));
+
+function LetterCapitalize (str) {
+  let arr = str.split(' ');
+  let string = arr.map(function (value) {
+    let first = value.charAt(0).toUpperCase();
+    let word = value.slice(1);
+    return first.concat(word);
+  });
+  return string.join(' ');
+}
+console.log(LetterCapitalize('hello world it'));
--- test/test.js
+++ test/test.js
@@ -1,17 +1,33 @@
 'use strict';
 var assert = require('assert');
 
-describe('getAvergeAge', function () {
-  let John = { name: 'John', age: 25 };
-  let Pete = { name: 'Pete', age: 30 };
-  let Mary = { name: 'Mary', age: 28 };
-  let arr = [John, Pete, Mary];
-
-  it('get Averge Age arr', function () {
-    assert.strictEqual(getAvergeAge(arr), 83);
+describe('SimpleAdding', function () {
+  it('Sum of previous numbers', function () {
+    assert.strictEqual(SimpleAdding('12'), 78);
   });
 });
 
-function getAvergeAge (arr) {
-  return arr.map(value => value.age).reduce((sum, current, index) => sum + current, 0);
+function SimpleAdding (num) {
+  let sum = 0;
+  let num1 = Number(num);
+  for (let i = 1; i <= num1; i++) {
+    sum += i;
+  }
+  return sum;
+}
+
+describe('LetterCapitalize', function () {
+  it('LetterCapitalize string hello wotld', function () {
+    assert.strictEqual(LetterCapitalize('hello world'), 'Hello World');
+  });
+});
+
+function LetterCapitalize (str) {
+  let arr = str.split(' ');
+  let string = arr.map(function (value) {
+    let first = value.charAt(0).toUpperCase();
+    let word = value.slice(1);
+    return first.concat(word);
+  });
+  return string.join(' ');
 }
